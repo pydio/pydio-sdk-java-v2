a0931d34d52a3cebe27ed13b0f65ed3039feee66
pydio <jabar@ajaxplorer.info>
2014-03-28 17:42:45 +0100

    removed all "throw Message"

--- Pydio SDK/src/io/pyd/sdk/client/transport/TransportFactory.java
+++ Pydio SDK/src/io/pyd/sdk/client/transport/TransportFactory.java
@@ -1,15 +1,21 @@
 package io.pyd.sdk.client.transport;
 
+import io.pyd.sdk.client.auth.CredentialsProvider;
+import io.pyd.sdk.client.utils.StateHolder;
+
 public class TransportFactory {
 
-	public static Transport getInstance(int mode){		
+	public static Transport getInstance(int mode, CredentialsProvider provider){
+		
+		
 		if(mode == Transport.MODE_RESTFUL){
 			return new RestTransport();
 		}else if(mode == Transport.MODE_SESSION){
-			return new SessionTransport();
+			return new SessionTransport(StateHolder.getInstance().getServer(), provider);
 		}else if(mode == Transport.MODE_MOCK){
 			return new MockTransport();
 		}
+		
 		return null;
 	}
 }
