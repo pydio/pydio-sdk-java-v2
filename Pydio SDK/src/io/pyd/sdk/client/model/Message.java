b3758af2bb890249ab384d3bdac7c76971f68d8b
pydio <jabar@ajaxplorer.info>
2014-03-28 17:58:02 +0100

    - Message creation from XML and event publishing - code attribute removed

--- Pydio SDK/src/io/pyd/sdk/client/model/Message.java
+++ Pydio SDK/src/io/pyd/sdk/client/model/Message.java
@@ -1,54 +1,86 @@
 package io.pyd.sdk.client.model;
 
-public class Message extends Exception{
+import java.util.ArrayList;
 
-	/**
-	 * 
-	 */
+import io.pyd.sdk.client.utils.DefaultEventBus;
+import io.pyd.sdk.client.utils.Pydio;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class Message{
+
 	
-	public final static int TYPE_LOCAL = 1;
-	public final static int TYPE_REMOTE = 2;
 	
-	
-	
+	public final static int SUCCESS = 1;
+	public final static int ERROR 	= 2;
+		
 	
 	private static final long serialVersionUID = 1L;
 	
 	
 	private String message;
-	private int code = -1;	
-	private int type = -1;
+	private String type;
 	
 	
-	public int getType() {
+	public String getType() {
 		return type;
 	}
-	
-	public int getCode() {
-		return code;
-	}
+
 	
 	public String getMessage() {
 		return message;
 	}
 	
-	
 	public void setMessage(String message){
 		this.message = message;
 	}
 	
-	public void setType(int type){
+	public void setType(String type){
 		this.type = type;
 	}
-	
-	public void setCode(int code){
-		this.code = code;
+
+	public static Message create(Document doc){
+		
+		org.w3c.dom.Node xml_message = doc.getElementsByTagName(Pydio.XML_MESSAGE).item(0);
+		
+		Message message = new Message();
+		message.setMessage(xml_message.getTextContent());
+		message.setType(xml_message.getAttributes().getNamedItem(Pydio.MESSAGE_PROPERTY_TYPE).getNodeValue());
+		
+		
+		NodeList removes = doc.getElementsByTagName(Pydio.NODE_DIFF_REMOVE).item(0).getChildNodes();
+		ArrayList<String> pathes = new ArrayList<String>();
+		for(int i = 0; i < removes.getLength(); i++){
+			pathes.add(removes.item(i).getAttributes().getNamedItem(Pydio.NODE_PROPERTY_FILENAME).getNodeValue());
+		}		
+		DefaultEventBus.bus().publish(new TreeRemoveEvent(pathes));
+		
+		
+		
+		NodeList adds = doc.getElementsByTagName(Pydio.NODE_DIFF_ADD).item(0).getChildNodes();
+		ArrayList<Node> nodes = new ArrayList<Node>();
+		for(int i = 0; i < adds.getLength(); i++){
+			nodes.add(NodeFactory.createNode(adds.item(i)));
+			
+		}
+		DefaultEventBus.bus().publish(new TreeAddEvent(nodes));
+		
+		
+		
+		NodeList updates = doc.getElementsByTagName(Pydio.NODE_DIFF_UPDATE).item(0).getChildNodes();
+		nodes = new ArrayList<Node>();
+		for(int i = 0; i < updates.getLength(); i++){			
+			nodes.add(NodeFactory.createNode(updates.item(i)));			
+		}
+		DefaultEventBus.bus().publish(new TreeUpdateEvent(nodes));
+		
+		
+		return message;
 	}
 	
-	
-	public static Message create(int code, int type, String message){
+	public static Message create(String type, String message){
 		Message m = new Message();
-		m.code = code;
 		m.type = type;
 		m.message = message;
 		return m;
