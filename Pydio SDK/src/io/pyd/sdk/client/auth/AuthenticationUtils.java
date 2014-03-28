390b0bebb0f175b20c42a0d68465a9276c8a046a
pydio <jabar@ajaxplorer.info>
2014-03-28 18:19:58 +0100

    md5 implementation

--- Pydio SDK/src/io/pyd/sdk/client/auth/AuthenticationUtils.java
+++ Pydio SDK/src/io/pyd/sdk/client/auth/AuthenticationUtils.java
@@ -1,5 +1,37 @@
 package io.pyd.sdk.client.auth;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class AuthenticationUtils {
+	
+	public static final String md5(final String s) {
+	    try {
+	        // Create MD5 Hash
+	        MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+	        digest.update(s.getBytes());
+	        byte messageDigest[] = digest.digest();
+	 
+	        // Create Hex String
+	        StringBuffer hexString = new StringBuffer();
+	        for (int i = 0; i < messageDigest.length; i++) {
+	            String h = Integer.toHexString(0xFF & messageDigest[i]);
+	            while (h.length() < 2)
+	                h = "0" + h;
+	            hexString.append(h);
+	        }
+	        return hexString.toString();
+	 
+	    } catch (NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    }
+	    return "";
+	}
+	
+	
+	public static String processPydioPassword(String password, String seed){
+		password = md5(password) + seed;
+		return md5(password);		
+	}
 	
 }
