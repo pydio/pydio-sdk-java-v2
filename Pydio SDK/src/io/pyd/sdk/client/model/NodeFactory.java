b798a9f15a8b5eab1a62183958b206ff51b1596d
pydio <jabar@ajaxplorer.info>
2014-03-28 17:57:14 +0100

    removed all "throws Message"

--- Pydio SDK/src/io/pyd/sdk/client/model/NodeFactory.java
+++ Pydio SDK/src/io/pyd/sdk/client/model/NodeFactory.java
@@ -6,23 +6,49 @@ import org.json.JSONObject;
 
 public class NodeFactory {
 	
-	public static Node createNode(int type, org.w3c.dom.Node xml, String specName) throws Message{
-		NodeSpec spec = NodeSpecContainer.getSpecInstance(specName);
+	public static Node createNode(int type, org.w3c.dom.Node xml, String specName){
+
 		Node node = instantiateNode(type);
-		node.initFromXml(xml);
-		node.setNodeSpec(spec);
+		if(xml != null) {
+			node.initFromXml(xml);
+		}
+		
+		if(specName != null){
+			NodeSpec spec = NodeSpecContainer.getSpecInstance(specName);
+			node.setNodeSpec(spec);
+		}
 		return node;
 	}
 	
-	public static Node createNode(int type, JSONObject json, String specName) throws Message{
+	public static Node createNode(int type, JSONObject json, String specName){
 		NodeSpec spec = NodeSpecContainer.getSpecInstance(specName);
 		Node node = instantiateNode(type);
-		node.initFromJson(json);
-		node.setNodeSpec(spec);
+		
+		if(json != null){
+			node.initFromJson(json);
+		}
+		
+		if(spec != null){
+			node.setNodeSpec(spec);
+		}		
 		return node;
 	}
+	
+	public static Node createNode(int type) {
+		return instantiateNode(type);
+	}
+	
+	public static Node createNode(org.w3c.dom.Node xml){
+		String nodename = xml.getNodeName(); 
+		if("repo".equals(nodename)) {
+			return createNode(Node.TYPE_REPOSITORY, xml, null);
+		}else if("tree".equals(nodename)){
+			return createNode(Node.TYPE_FILE, xml, null);
+		}
+		return null;
+	}
 
-	public static Node createNode(int type, Properties prop, String specName) throws Message{
+	public static Node createNode(int type, Properties prop, String specName){
 		NodeSpec spec = NodeSpecContainer.getSpecInstance(specName);
 		Node node = instantiateNode(type);
 		node.initFromProperties(prop);
@@ -30,21 +56,22 @@ public class NodeFactory {
 		return node;
 	}
 	
-	private static Node instantiateNode(int type) throws Message{
-		switch (type) {		
-		case Node.TYPE_FILE:
-			return new FileNode();
-			
-		case Node.TYPE_REPOSITORY:
-			return new RepositoryNode();
-			
-		case Node.TYPE_SERVER:			
-			return new ServerNode();
-			
-		case Node.TYPE_VIRTUAL:
-			return new VirtualNode();
-		default:
-			throw new Message();	
+	
+	private static Node instantiateNode(int type){
+		switch (type) {			
+			case Node.TYPE_FILE:
+				return new FileNode();
+				
+			case Node.TYPE_REPOSITORY:
+				return new RepositoryNode();
+				
+			case Node.TYPE_SERVER:
+				return new ServerNode();
+				
+			case Node.TYPE_VIRTUAL:
+				return new VirtualNode();
+			default:
+				return null;	
 		}
 	}
 }
