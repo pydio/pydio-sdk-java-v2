ce836b7ec5cfa993b7be60987af1530828d27121
jabar <jabar@ajaxplorer.info>
2015-11-10 14:27:42 +0100

    Rewrote Progresslistener for better interruption handling during upload, now returns boolean

---
+++ src/main/java/pydio/sdk/java/utils/UploadStopNotifierProgressListener.java
@@ -0,0 +1,8 @@
+package pydio.sdk.java.utils;
+
+/**
+ * Created by jabar on 10/11/2015.
+ */
+public interface UploadStopNotifierProgressListener {
+    public boolean onProgress(long progress);
+}
