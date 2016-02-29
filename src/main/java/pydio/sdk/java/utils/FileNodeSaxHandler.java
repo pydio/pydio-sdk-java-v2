22c63912a84124191c301d690ca4b5451b0a8285
jabar <jabar@ajaxplorer.info>
2016-02-29 17:41:04 +0100

    Properly handled pagination

--- src/main/java/pydio/sdk/java/utils/FileNodeSaxHandler.java
+++ src/main/java/pydio/sdk/java/utils/FileNodeSaxHandler.java
@@ -9,65 +9,77 @@ import java.util.Properties;
 import pydio.sdk.java.model.Node;
 import pydio.sdk.java.model.NodeFactory;
 import pydio.sdk.java.model.NodeHandler;
+import pydio.sdk.java.model.TreeNode;
 
 /**
  * Created by pydio on 09/02/2015.
  */
 public class FileNodeSaxHandler extends DefaultHandler {
 
-    boolean inside_tree = false;
-    String inner_element = "";
-    int count = 0, offset, max;
-    NodeHandler handler;
+    boolean mInsideTree = false;
+    String mInnerElement = "";
 
+    public int mParsedCount = 0;
+    public boolean mPagination = false;
+    public int mPaginationTotalItem;
+    public int mPaginationTotalPage;
+    public int mPaginationCurrentPage;
+    public TreeNode mRootNode;
+
+    NodeHandler mHandler;
     Properties p = null;
+
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        count++;
-        if(max != -1 && (count < offset || count > max)){
-            return;
+        if("pagination".equals(qName)){
+            mPagination = true;
+            mPaginationTotalItem = Integer.parseInt(attributes.getValue("count"));
+            mPaginationTotalPage = Integer.parseInt(attributes.getValue("total"));
+            mPaginationCurrentPage = Integer.parseInt(attributes.getValue("current"));
         }
+
         if("tree".equals(qName)){
-            inside_tree = true;
+            mInsideTree = true;
+            mParsedCount++;
             p = new Properties();
             for(int i = 0; i < attributes.getLength(); i++){
                 p.setProperty(attributes.getLocalName(i), attributes.getValue(i));
             }
-        }
-        if(!inside_tree) return;
-        if("label".equals(qName.toLowerCase()) || "description".equals(qName.toLowerCase())){
-            inner_element = qName;
-        }
-    }
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-        if(max != -1 && (count < offset || count > max)){
-            return;
+            if(mParsedCount == 1){
+                if("".equals(p.getProperty(Pydio.NODE_PROPERTY_FILENAME))){
+                    p.setProperty(Pydio.NODE_PROPERTY_FILENAME, "/");
+                }
+                mRootNode = (TreeNode) NodeFactory.createNode(Node.TYPE_TREE, p);
+                p = null;
+            }
         }
 
-        if(inside_tree && "tree".equals(qName)){
-            handler.processNode(NodeFactory.createNode(Node.TYPE_TREE, p));
-            p = null;
-            inside_tree = false;
+        if(!mInsideTree) return;
+
+        if("label".equals(qName.toLowerCase()) || "description".equals(qName.toLowerCase())){
+            mInnerElement = qName;
+        }
+    }
+
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if(mInsideTree && "tree".equals(qName)){
+            if(p != null) {
+                mHandler.processNode(NodeFactory.createNode(Node.TYPE_TREE, p));
+                p = null;
+            }
+            mInsideTree = false;
             return;
         }
     }
 
     public void characters(char ch[], int start, int length) throws SAXException {
-        if(max != -1 && (count < offset || count > max)){
-            return;
-        }
-        if(inside_tree){
-            p.setProperty(inner_element, new String(ch, start, length));
+        if(mInsideTree){
+            p.setProperty(mInnerElement, new String(ch, start, length));
         }
     }
 
-    public void endDocument() throws SAXException {
-        if(max != -1)
-            handler.processNode(null);
-    }
+    public void endDocument() throws SAXException {}
 
-    public FileNodeSaxHandler(NodeHandler nodeHandler, int offset, int max){
-        this.handler = nodeHandler;
-        this.offset = offset;
-        this.max = max;
+    public FileNodeSaxHandler(NodeHandler nodeHandler){
+        this.mHandler = nodeHandler;
     }
 }
