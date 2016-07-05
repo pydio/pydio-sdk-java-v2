53b653d24f06d66ebeb2ad4628f142abfe94827f
jabar <jabar@ajaxplorer.info>
2016-07-05 14:18:55 +0200

    Introduced class to handle high level application information

---
+++ src/main/java/pydio/sdk/java/utils/ApplicationData.java
@@ -0,0 +1,12 @@
+package pydio.sdk.java.utils;
+
+/**
+ * Created by jabar on 24/06/2016.
+ */
+public class ApplicationData {
+
+    public static String name = "";
+    public static String version = "";
+    public static int versionCode = 1;
+
+}
