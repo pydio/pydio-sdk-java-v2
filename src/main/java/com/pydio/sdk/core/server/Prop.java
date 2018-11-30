b23e8debdd437b67d3ef14a7db226c7ba3ae7dcc
jabar <jabar@ajaxplorer.info>
2018-11-30 16:49:26 +0100

    Added constants

---
+++ src/main/java/com/pydio/sdk/core/server/Prop.java
@@ -0,0 +1,12 @@
+package com.pydio.sdk.core.server;
+
+public class Prop {
+    public static final String uploadMaxSize = "UPLOAD_MAX_SIZE";
+    public static final String appTitle = "APPLICATION_TITLE";
+    public static final String noPasswordSave = "MOBILE_SECURITY_FORCE_DONTSAVEPASS";
+    public static final String forcePinCode = "MOBILE_SECURITY_FORCE_PIN_CODE";
+    public static final String forceAuthEvery = "MOBILE_SECURITY_FORCE_REAUTH";
+    public static final String doNotShareOutside = "MOBILE_SECURITY_OUTSIDE_SHARE";
+    public static final String offlineDisabled = "MOBILE_SECURITY_DISABLE_OFFLINE";
+    public static final String backupDisabled = "MOBILE_SECURITY_DISABLE_BACKUP";
+}
